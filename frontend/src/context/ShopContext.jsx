fc04c80c30e9b63a16f4d815a0a21e4916bc6c19
Om Bilthere <ombilthere953@gmail.com>
2026-03-21 20:00:55 +0530

    feat: Implement admin panel with authentication and product management
    - Added Login component for admin authentication with token storage.
    - Updated App component to manage routes and conditional rendering based on authentication state.
    - Enhanced Navbar to include logout functionality.
    - Created Add page for adding new products with image upload and form handling.
    - Developed List page to display all products with delete functionality.
    - Integrated axios for API calls to backend for product management.
    - Updated ShopContext to fetch products from backend and manage state.
    - Improved styling and layout across components for better user experience.

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,10 +1,10 @@
 import { createContext, useEffect, useState } from "react";
 
-import { products } from "../assets/assets";
-
 import { toast } from 'react-toastify'  
 
 import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
 
 export const ShopContext = createContext();
 
@@ -13,12 +13,16 @@ const ShopContextProvider =({children})=>{
     const currency = '$';
 
     const delivery_fee= 10;
+    
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [search, setSearch] = useState('')
 
     const [showSearch, setShowSearch] = useState(false)
    
     const [cartItems, setCartItems] = useState({})
+
+    const [products, setProducts] = useState([])
 
     const navigate = useNavigate()
     
@@ -122,10 +126,33 @@ const ShopContextProvider =({children})=>{
                     }
         
        }
-        return totalAmount  
+       
+    }
+    return totalAmount  
+ }
+
+    const getProductsData = async () => {
+        try {
+            
+            const response = await axios.get(backendUrl +'/api/product/list')
+            if(response.data.success) {
+                setProducts(response.data.products)
+            }
+            else {
+                toast.error(response.data.message)
+            }
+    
+            
+           
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 
- }
+    useEffect(()=>{
+        getProductsData()
+    },[])    
 
     const value = {
 
@@ -135,7 +162,7 @@ const ShopContextProvider =({children})=>{
       
       cartItems , addToCart , getCartCount , updateQuantity
 
-      , getCartAmount , navigate
+      , getCartAmount , navigate , backendUrl
       
     }
 
